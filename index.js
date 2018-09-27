14d8876d01d3a8b2cda90bc000fe1b3023e60499
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2018-09-27 12:50:27 +0200

    Add bot auth client with certifs as secret

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { send, json } = require("micro");
+const { send, json } = require('micro');
+const { botAuthClient, botClient } = require('./bot-client');
 
 module.exports = async (req, res) => {
   const statusCode = 200;
@@ -9,6 +10,14 @@ module.exports = async (req, res) => {
   } catch (err) {
     data = "This is a trello webhook";
   }
+
+  let token;
+  try {
+    data = token = await botAuthClient.post();
+  } catch (err) {
+    data = err
+  }
+
 
   console.log("-----> Debug data", data);
 
